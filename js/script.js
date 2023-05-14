0807e774f8899362690f04ebe38a7e94e73f0ea6
konoozgamal <aya476771@gmail.com>
2023-05-14 18:18:56 +0300

    Saving to Github

--- js/script.js
+++ js/script.js
@@ -36,47 +36,42 @@ function scrollActive(){
             document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.add('active')
         }else{
             document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.remove('active')
-        }
-    })
-}
+        };
+    });
+};
 //========================= Scroll Reveal =====================//
-
 
 const sr = ScrollReveal({
   origin: "top",
-  distance: "80px",
+  distance: "30px",
   duration: 2000,
   reset: true,
 });
 // Scroll Home
 sr.reveal(".home_title", {});
 sr.reveal(".home_scroll", { delay: 200 });
-sr.reveal(".home_img", { origin: "right", delay: 400 });
+sr.reveal(".home_img", { origin: "right", delay: 200});
 
 // Scroll about
 
-sr.reveal(".about_img", { origin: "left", delay: 500 });
+sr.reveal(".about_img", {delay: 400 });
 sr.reveal(".about_subtitle", { delay: 300 });
 sr.reveal(".about_profession", { delay: 400 });
-sr.reveal(".about_text", { delay: 500 });
-sr.reveal(".list_5", { delay: 600, interval: 200 });
+sr.reveal(".about_text", { delay: 400 });
+sr.reveal(".list_5", { delay: 400, interval: 200 });
 
 // Scroll skills
 
-sr.reveal(".skills_subtitle", { delay: 300 });
-sr.reveal(".skills_name", { distance: "20px", delay: 50, interval: 100 });
+sr.reveal(".skills_subtitle", {});
+sr.reveal(".skills_name", { distance: "20px", delay: 40, interval: 100 });
 sr.reveal(".skills_img", { delay: 400 });
 
-
 // Scroll PORTFOLIO
-
 sr.reveal('.portfolio_img', {interval: 200})
 
 // Scroll CONTACT
 
 sr.reveal('.contact_subtitle', {})
 sr.reveal('.contact_text', {interval: 200})
-sr.reveal('.contact_input', {delay: 400})
-sr.reveal('.contact_button', {delay: 600})
-
-
+sr.reveal('.contact_input', {delay: 300})
+sr.reveal('.contact_button', {delay: 400})
